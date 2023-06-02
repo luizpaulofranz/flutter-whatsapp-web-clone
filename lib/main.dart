e43ae99fa1f288ce616d1496e119e56695e74ea1
Luiz Paulo Franz <luizpaulofranz@gmail.com>
2023-06-02 17:18:57 -0300

    Adds firebase init on the right place.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_web_clone/screens/login_register.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyBbfoasdgKz5wvcKk21NgQbXGf9GebzNA8',
+      appId: '1:484346022413:web:eb729bd0f5b5e3987a2344',
+      messagingSenderId: '484346022413',
+      projectId: 'flutter-whatsapp-1d294',
+      authDomain: 'flutter-whatsapp-1d294.firebaseapp.com',
+      storageBucket: 'flutter-whatsapp-1d294.appspot.com',
+    ),
+  );
   runApp(const MainApp());
 }
 
